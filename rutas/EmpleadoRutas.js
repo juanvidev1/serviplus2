2a301b1733ee0f9bea27883fe64304284eaa0e71
juanvidev1 <juanvidev1@gmail.com>
2022-10-25 23:58:23 -0500

    Corrección empleados

--- rutas/EmpleadoRutas.js
+++ rutas/EmpleadoRutas.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 
 router.get("/", empleadoOperaciones.listarEmpleados);
 // router.get("/:id", empleadoOperaciones.listarCliente);
-//router.post("/", empleadoOperaciones.crearCliente);
+router.post("/", empleadoOperaciones.crearEmpleado);
 // router.put("/:id", empleadoOperaciones.actualizarCliente);
 
 module.exports = router;
