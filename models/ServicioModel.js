c5da19fc22722cafab5baafdc53102312f971d1a
juanvidev1 <juanvidev1@gmail.com>
2022-11-12 13:41:40 -0500

    cambio en modelo de servicio

--- models/ServicioModel.js
+++ models/ServicioModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const serviceSchema = mongoose.Schema({
     nombre_servicio: {type: String, maxLength: 60, required: true, unique: false},
     descripcion_servicio: {type: String, maxLength: 300, required: true, unique: false},
-    tipo_servicio: {type: String, maxLength: 40, required: true, unique: false},
+    tipo_servicio: {type: String, maxLength: 40, required: true, false: false},
     comentarios: {type: String, maxLength: 300, required: false, unique: false}
 });
 
